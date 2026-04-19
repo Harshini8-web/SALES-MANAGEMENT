89f900022716cbb05ce118ee4fa2ca2b4b8cba13
dhatrisriram <dhatrisriram.1107@gmail.com>
2026-04-19 15:47:48 +0530

    Integrated with Order ss

--- Sales-Management-main/Sales_Management/leads/db/DBConnection.java
+++ Sales-Management-main/Sales_Management/leads/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/polymorphs";
     private static final String USER = "root";
-    private static final String PASSWORD = "Harshu200412@";
+    private static final String PASSWORD = "Dhatri2005@$";
 
     private DBConnection() {
     }
